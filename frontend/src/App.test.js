81648fa173f0ee8bec0e6d49d1dfea3f733e95a2
matheusfefagundes <matheusferreirafagundes23@gmail.com>
2025-10-19 21:51:29 -0300

    SCRUM-10: adicionar testes unitários no frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renderiza título da lista", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Lista de Itens/i)).toBeInTheDocument();
 });
